a5da1527a837fabcb70b8e4ae252b340dc020996
h-tacayama <h.tacayama@gmail.com>
2025-02-12 22:52:40 +0900

    perf: Reduce memory usage

--- src/InlineSqlSharp/SqlElement/AliasName.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace InlineSqlSharp;
-
-public sealed class AliasName(string name)
-{
-	public string Name { get; } = ArgumentValidator.NotEmpty(name);
-
-	public override string ToString() => Name;
-}
