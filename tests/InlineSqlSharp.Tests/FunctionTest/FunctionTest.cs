75edfd3dad53c0ea8d36ff5b6639e8410eb1885f
h-tacayama <h.tacayama@gmail.com>
2025-03-02 00:35:30 +0900

    test: Split FunctionTest class into multiple files

--- tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
@@ -3,38 +3,8 @@ using static InlineSqlSharp.SqlWordbook;
 
 namespace InlineSqlSharp.Tests;
 
-public class FunctionTest
+public partial class FunctionTest
 {
-	private readonly test_table _t = new("t");
-
-	[Fact]
-	public void SELECT_AVG()
-	{
-		SqlCommand sql =
-			SELECT(AVG(_t.code))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("AVG(t.code)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_AVG_DISTINCT()
-	{
-		SqlCommand sql =
-			SELECT(AVG(DISTINCT, _t.code))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("AVG(DISTINCT t.code)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
 	[Fact]
 	public void SELECT_CONCAT()
 	{
---
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTestA.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+using static InlineSqlSharp.SqlWordbook;
+
+namespace InlineSqlSharp.Tests;
+
+public partial class FunctionTest
+{
+	private readonly test_table _t = new("t");
+
+	[Fact]
+	public void SELECT_AVG()
+	{
+		SqlCommand sql =
+			SELECT(AVG(_t.code))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("AVG(t.code)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+
+	[Fact]
+	public void SELECT_AVG_DISTINCT()
+	{
+		SqlCommand sql =
+			SELECT(AVG(DISTINCT, _t.code))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("AVG(DISTINCT t.code)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+}
