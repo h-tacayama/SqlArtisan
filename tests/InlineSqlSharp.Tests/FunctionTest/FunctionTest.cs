19a84c1ffdc42e307dafaf654f025a0e29ecf25f
h-tacayama <h.tacayama@gmail.com>
2025-03-02 00:42:51 +0900

    test: Split FunctionTest class into multiple files

--- tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
@@ -104,20 +104,6 @@ public partial class FunctionTest
 	}
 
 	[Fact]
-	public void SELECT_TO_DATE()
-	{
-		SqlCommand sql =
-			SELECT(TO_DATE(L("2001/02/03"), "YYYY/MM/DD"))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("TO_DATE('2001/02/03', 'YYYY/MM/DD')");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
 	public void SELECT_SUM()
 	{
 		SqlCommand sql =
@@ -141,34 +127,6 @@ public partial class FunctionTest
 		StringBuilder expected = new();
 		expected.AppendLine("SELECT");
 		expected.Append("SUM(DISTINCT t.code)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_TRIM()
-	{
-		SqlCommand sql =
-			SELECT(TRIM(_t.name))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("TRIM(t.name)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_TRIM_TrimChar()
-	{
-		SqlCommand sql =
-			SELECT(TRIM(_t.name, L("a")))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("TRIM(BOTH 'a' FROM t.name)");
 
 		Assert.Equal(expected.ToString(), sql.Statement);
 	}
---
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTestT.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using static InlineSqlSharp.SqlWordbook;
+
+namespace InlineSqlSharp.Tests;
+
+public partial class FunctionTest
+{
+	[Fact]
+	public void SELECT_TO_DATE()
+	{
+		SqlCommand sql =
+			SELECT(TO_DATE(L("2001/02/03"), "YYYY/MM/DD"))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("TO_DATE('2001/02/03', 'YYYY/MM/DD')");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+
+	[Fact]
+	public void SELECT_TRIM()
+	{
+		SqlCommand sql =
+			SELECT(TRIM(_t.name))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("TRIM(t.name)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+
+	[Fact]
+	public void SELECT_TRIM_TrimChar()
+	{
+		SqlCommand sql =
+			SELECT(TRIM(_t.name, L("a")))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("TRIM(BOTH 'a' FROM t.name)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+}
