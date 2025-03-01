be4580acbff09cfd0f418024ca3360581eb3ec56
h-tacayama <h.tacayama@gmail.com>
2025-03-02 00:45:06 +0900

    test: Split FunctionTest class into multiple files

--- tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
@@ -102,32 +102,4 @@ public partial class FunctionTest
 
 		Assert.Equal(expected.ToString(), sql.Statement);
 	}
-
-	[Fact]
-	public void SELECT_SUM()
-	{
-		SqlCommand sql =
-			SELECT(SUM(_t.code))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("SUM(t.code)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SUM_DISTINCT()
-	{
-		SqlCommand sql =
-			SELECT(SUM(DISTINCT, _t.code))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("SUM(DISTINCT t.code)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
 }
---
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTestS.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using static InlineSqlSharp.SqlWordbook;
+
+namespace InlineSqlSharp.Tests;
+
+public partial class FunctionTest
+{
+	[Fact]
+	public void SELECT_SUM()
+	{
+		SqlCommand sql =
+			SELECT(SUM(_t.code))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("SUM(t.code)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+
+	[Fact]
+	public void SELECT_SUM_DISTINCT()
+	{
+		SqlCommand sql =
+			SELECT(SUM(DISTINCT, _t.code))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("SUM(DISTINCT t.code)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+}
