a9822d6a15775ac40e09f6a65343845884701b93
h-tacayama <h.tacayama@gmail.com>
2025-03-02 00:36:49 +0900

    test: Split FunctionTest class into multiple files

--- tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
@@ -6,48 +6,6 @@ namespace InlineSqlSharp.Tests;
 public partial class FunctionTest
 {
 	[Fact]
-	public void SELECT_CONCAT()
-	{
-		SqlCommand sql =
-			SELECT(CONCAT(_t.name, L("a"), L("b")))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("CONCAT(t.name, 'a', 'b')");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_COUNT()
-	{
-		SqlCommand sql =
-			SELECT(COUNT(_t.code))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("COUNT(t.code)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_COUNT_DISTINCT()
-	{
-		SqlCommand sql =
-			SELECT(COUNT(DISTINCT, _t.code))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("COUNT(DISTINCT t.code)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
 	public void SELECT_INSTR()
 	{
 		SqlCommand sql =
---
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTestC.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using static InlineSqlSharp.SqlWordbook;
+
+namespace InlineSqlSharp.Tests;
+
+public partial class FunctionTest
+{
+	[Fact]
+	public void SELECT_CONCAT()
+	{
+		SqlCommand sql =
+			SELECT(CONCAT(_t.name, L("a"), L("b")))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("CONCAT(t.name, 'a', 'b')");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+
+	[Fact]
+	public void SELECT_COUNT()
+	{
+		SqlCommand sql =
+			SELECT(COUNT(_t.code))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("COUNT(t.code)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+
+	[Fact]
+	public void SELECT_COUNT_DISTINCT()
+	{
+		SqlCommand sql =
+			SELECT(COUNT(DISTINCT, _t.code))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("COUNT(DISTINCT t.code)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+}
