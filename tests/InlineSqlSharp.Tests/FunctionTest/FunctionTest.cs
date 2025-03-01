2e94c1c0720c803aaaea4221a77dadd9ecc71903
h-tacayama <h.tacayama@gmail.com>
2025-03-02 00:37:55 +0900

    test: Split FunctionTest class into multiple files

--- tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTest.cs
@@ -6,58 +6,6 @@ namespace InlineSqlSharp.Tests;
 public partial class FunctionTest
 {
 	[Fact]
-	public void SELECT_INSTR()
-	{
-		SqlCommand sql =
-			SELECT(INSTR(_t.name, L("abc")))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.Append("INSTR(t.name, 'abc')");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_INSTR_Position()
-	{
-		SqlCommand sql =
-			SELECT(
-				INSTR(_t.name, L("abc"), L(2)),
-				INSTR(_t.name, L("abc"), L(1)),
-				INSTR(_t.name, L("abc"), L(-1)))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("INSTR(t.name, 'abc', 2)");
-		expected.AppendLine(", INSTR(t.name, 'abc', 1)");
-		expected.Append(", INSTR(t.name, 'abc', -1)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_INSTR_Occurrence()
-	{
-		SqlCommand sql =
-			SELECT(
-				INSTR(_t.name, L("abc"), L(1), L(1)),
-				INSTR(_t.name, L("abc"), L(1), L(2)),
-				INSTR(_t.name, L("abc"), L(2), L(-1)))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("INSTR(t.name, 'abc', 1, 1)");
-		expected.AppendLine(", INSTR(t.name, 'abc', 1, 2)");
-		expected.Append(", INSTR(t.name, 'abc', 2, -1)");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
 	public void SELECT_LENGTH()
 	{
 		SqlCommand sql =
---
+++ tests/InlineSqlSharp.Tests/FunctionTest/FunctionTestI.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using static InlineSqlSharp.SqlWordbook;
+
+namespace InlineSqlSharp.Tests;
+
+public partial class FunctionTest
+{
+	[Fact]
+	public void SELECT_INSTR()
+	{
+		SqlCommand sql =
+			SELECT(INSTR(_t.name, L("abc")))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.Append("INSTR(t.name, 'abc')");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+
+	[Fact]
+	public void SELECT_INSTR_Position()
+	{
+		SqlCommand sql =
+			SELECT(
+				INSTR(_t.name, L("abc"), L(2)),
+				INSTR(_t.name, L("abc"), L(1)),
+				INSTR(_t.name, L("abc"), L(-1)))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.AppendLine("INSTR(t.name, 'abc', 2)");
+		expected.AppendLine(", INSTR(t.name, 'abc', 1)");
+		expected.Append(", INSTR(t.name, 'abc', -1)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+
+	[Fact]
+	public void SELECT_INSTR_Occurrence()
+	{
+		SqlCommand sql =
+			SELECT(
+				INSTR(_t.name, L("abc"), L(1), L(1)),
+				INSTR(_t.name, L("abc"), L(1), L(2)),
+				INSTR(_t.name, L("abc"), L(2), L(-1)))
+			.Build();
+
+		StringBuilder expected = new();
+		expected.AppendLine("SELECT");
+		expected.AppendLine("INSTR(t.name, 'abc', 1, 1)");
+		expected.AppendLine(", INSTR(t.name, 'abc', 1, 2)");
+		expected.Append(", INSTR(t.name, 'abc', 2, -1)");
+
+		Assert.Equal(expected.ToString(), sql.Statement);
+	}
+}
