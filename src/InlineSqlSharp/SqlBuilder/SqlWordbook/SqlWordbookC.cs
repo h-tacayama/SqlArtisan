287a859a6075fb7410a1153d8185b6f20b53f66b
h-tacayama <h.tacayama@gmail.com>
2025-03-07 06:11:56 +0900

    feat: Change the case expression factory's argument variation from overrides to an array

--- src/InlineSqlSharp/SqlBuilder/SqlWordbook/SqlWordbookC.cs
+++ src/InlineSqlSharp/SqlBuilder/SqlWordbook/SqlWordbookC.cs
@@ -3,130 +3,40 @@
 public static partial class SqlWordbook
 {
 	public static CharacterSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<CharacterExpr> whenClause,
+		SearchedCaseWhenClause<CharacterExpr>[] whenClauses,
 		CaseElseExpr<CharacterExpr> elseExpr) =>
-		new([whenClause], elseExpr);
-
-	public static CharacterSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<CharacterExpr> whenClause1,
-		SearchedCaseWhenClause<CharacterExpr> whenClause2,
-		CaseElseExpr<CharacterExpr> elseExpr) =>
-		new([whenClause1, whenClause2], elseExpr);
-
-	public static CharacterSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<CharacterExpr> whenClause1,
-		SearchedCaseWhenClause<CharacterExpr> whenClause2,
-		SearchedCaseWhenClause<CharacterExpr> whenClause3,
-		CaseElseExpr<CharacterExpr> elseExpr) =>
-		new([whenClause1, whenClause2, whenClause3], elseExpr);
+		new(whenClauses, elseExpr);
 
 	public static DateTimeSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<DateTimeExpr> whenClause,
+		SearchedCaseWhenClause<DateTimeExpr>[] whenClauses,
 		CaseElseExpr<DateTimeExpr> elseExpr) =>
-		new([whenClause], elseExpr);
-
-	public static DateTimeSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<DateTimeExpr> whenClause1,
-		SearchedCaseWhenClause<DateTimeExpr> whenClause2,
-		CaseElseExpr<DateTimeExpr> elseExpr) =>
-		new([whenClause1, whenClause2], elseExpr);
-
-	public static DateTimeSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<DateTimeExpr> whenClause1,
-		SearchedCaseWhenClause<DateTimeExpr> whenClause2,
-		SearchedCaseWhenClause<DateTimeExpr> whenClause3,
-		CaseElseExpr<DateTimeExpr> elseExpr) =>
-		new([whenClause1, whenClause2, whenClause3], elseExpr);
+		new(whenClauses, elseExpr);
 
 	public static NumericSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<NumericExpr> whenClause,
+		SearchedCaseWhenClause<NumericExpr>[] whenClauses,
 		CaseElseExpr<NumericExpr> elseExpr) =>
-		new([whenClause], elseExpr);
-
-	public static NumericSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<NumericExpr> whenClause1,
-		SearchedCaseWhenClause<NumericExpr> whenClause2,
-		CaseElseExpr<NumericExpr> elseExpr) =>
-		new([whenClause1, whenClause2], elseExpr);
-
-	public static NumericSearchedCaseExpr CASE(
-		SearchedCaseWhenClause<NumericExpr> whenClause1,
-		SearchedCaseWhenClause<NumericExpr> whenClause2,
-		SearchedCaseWhenClause<NumericExpr> whenClause3,
-		CaseElseExpr<NumericExpr> elseExpr) =>
-		new([whenClause1, whenClause2, whenClause3], elseExpr);
+		new(whenClauses, elseExpr);
 
 	public static CharacterSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
 		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, CharacterExpr> whenClause,
+		SimpleCaseWhenClause<TComparisonExpr, CharacterExpr>[] whenClauses,
 		CaseElseExpr<CharacterExpr> elseExpr)
 		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause], elseExpr);
-
-	public static CharacterSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
-		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, CharacterExpr> whenClause1,
-		SimpleCaseWhenClause<TComparisonExpr, CharacterExpr> whenClause2,
-		CaseElseExpr<CharacterExpr> elseExpr)
-		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause1, whenClause2], elseExpr);
-
-	public static CharacterSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
-		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, CharacterExpr> whenClause1,
-		SimpleCaseWhenClause<TComparisonExpr, CharacterExpr> whenClause2,
-		SimpleCaseWhenClause<TComparisonExpr, CharacterExpr> whenClause3,
-		CaseElseExpr<CharacterExpr> elseExpr)
-		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause1, whenClause2, whenClause3], elseExpr);
+			new(expr, whenClauses, elseExpr);
 
 	public static DateTimeSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
 		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, DateTimeExpr> whenClause,
+		SimpleCaseWhenClause<TComparisonExpr, DateTimeExpr>[] whenClauses,
 		CaseElseExpr<DateTimeExpr> elseExpr)
 		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause], elseExpr);
-
-	public static DateTimeSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
-		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, DateTimeExpr> whenClause1,
-		SimpleCaseWhenClause<TComparisonExpr, DateTimeExpr> whenClause2,
-		CaseElseExpr<DateTimeExpr> elseExpr)
-		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause1, whenClause2], elseExpr);
-
-	public static DateTimeSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
-		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, DateTimeExpr> whenClause1,
-		SimpleCaseWhenClause<TComparisonExpr, DateTimeExpr> whenClause2,
-		SimpleCaseWhenClause<TComparisonExpr, DateTimeExpr> whenClause3,
-		CaseElseExpr<DateTimeExpr> elseExpr)
-		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause1, whenClause2, whenClause3], elseExpr);
+			new(expr, whenClauses, elseExpr);
 
 	public static NumericSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
 		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, NumericExpr> whenClause,
+		SimpleCaseWhenClause<TComparisonExpr, NumericExpr>[] whenClauses,
 		CaseElseExpr<NumericExpr> elseExpr)
 		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause], elseExpr);
-
-	public static NumericSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
-		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, NumericExpr> whenClause1,
-		SimpleCaseWhenClause<TComparisonExpr, NumericExpr> whenClause2,
-		CaseElseExpr<NumericExpr> elseExpr)
-		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause1, whenClause2], elseExpr);
-
-	public static NumericSimpleCaseExpr<TComparisonExpr> CASE<TComparisonExpr>(
-		TComparisonExpr expr,
-		SimpleCaseWhenClause<TComparisonExpr, NumericExpr> whenClause1,
-		SimpleCaseWhenClause<TComparisonExpr, NumericExpr> whenClause2,
-		SimpleCaseWhenClause<TComparisonExpr, NumericExpr> whenClause3,
-		CaseElseExpr<NumericExpr> elseExpr)
-		where TComparisonExpr : IDataExpr =>
-			new(expr, [whenClause1, whenClause2, whenClause3], elseExpr);
+			new(expr, whenClauses, elseExpr);
 
 	public static ConcatFunction CONCAT(
 		CharacterExpr primary,
--- tests/InlineSqlSharp.Tests/CaseTest.cs
+++ tests/InlineSqlSharp.Tests/CaseTest.cs
@@ -8,33 +8,15 @@ public class CaseTest
 	private readonly test_table _t = new("t");
 
 	[Fact]
-	public void SELECT_SEARCH_CASE_Character_1()
+	public void SELECT_SEARCH_CASE_Character()
 	{
 		SqlCommand sql =
 			SELECT(
 				CASE(
-					WHEN(_t.name == L("a")).THEN(L("b")),
-					ELSE(L("z"))))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("WHEN (t.name = 'a') THEN 'b'");
-		expected.AppendLine("ELSE 'z'");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SEARCH_CASE_Character_2()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					WHEN(_t.name == L("a")).THEN(L("b")),
-					WHEN(_t.name == L("c")).THEN(L("d")),
+					[
+						WHEN(_t.name == L("a")).THEN(L("b")),
+						WHEN(_t.name == L("c")).THEN(L("d")),
+					],
 					ELSE(L("z"))))
 			.Build();
 
@@ -50,57 +32,15 @@ public class CaseTest
 	}
 
 	[Fact]
-	public void SELECT_SEARCH_CASE_Character_3()
+	public void SELECT_SEARCH_CASE_DateTime()
 	{
 		SqlCommand sql =
 			SELECT(
 				CASE(
-					WHEN(_t.name == L("a")).THEN(L("b")),
-					WHEN(_t.name == L("c")).THEN(L("d")),
-					WHEN(_t.name == L("e")).THEN(L("f")),
-					ELSE(L("z"))))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("WHEN (t.name = 'a') THEN 'b'");
-		expected.AppendLine("WHEN (t.name = 'c') THEN 'd'");
-		expected.AppendLine("WHEN (t.name = 'e') THEN 'f'");
-		expected.AppendLine("ELSE 'z'");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SEARCH_CASE_DateTime_1()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					WHEN(_t.code == L(1)).THEN(TO_DATE(L("2001/02/03"), L("YYYY/MM/DD"))),
-					ELSE(_t.created_at)))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("WHEN (t.code = 1) THEN TO_DATE('2001/02/03\', 'YYYY/MM/DD')");
-		expected.AppendLine("ELSE t.created_at");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SEARCH_CASE_DateTime_2()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					WHEN(_t.code == L(1)).THEN(TO_DATE(L("2001/02/03"), L("YYYY/MM/DD"))),
-					WHEN(_t.code == L(2)).THEN(TO_DATE(L("2004/05/06"), L("YYYY/MM/DD"))),
+					[
+						WHEN(_t.code == L(1)).THEN(TO_DATE(L("2001/02/03"), L("YYYY/MM/DD"))),
+						WHEN(_t.code == L(2)).THEN(TO_DATE(L("2004/05/06"), L("YYYY/MM/DD"))),
+					],
 					ELSE(_t.created_at)))
 			.Build();
 
@@ -116,57 +56,15 @@ public class CaseTest
 	}
 
 	[Fact]
-	public void SELECT_SEARCH_CASE_DateTime_3()
+	public void SELECT_SEARCH_CASE_Numeric()
 	{
 		SqlCommand sql =
 			SELECT(
 				CASE(
-					WHEN(_t.code == L(1)).THEN(TO_DATE(L("2001/02/03"), L("YYYY/MM/DD"))),
-					WHEN(_t.code == L(2)).THEN(TO_DATE(L("2004/05/06"), L("YYYY/MM/DD"))),
-					WHEN(_t.code == L(3)).THEN(TO_DATE(L("2007/08/09"), L("YYYY/MM/DD"))),
-					ELSE(_t.created_at)))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("WHEN (t.code = 1) THEN TO_DATE('2001/02/03', 'YYYY/MM/DD')");
-		expected.AppendLine("WHEN (t.code = 2) THEN TO_DATE('2004/05/06', 'YYYY/MM/DD')");
-		expected.AppendLine("WHEN (t.code = 3) THEN TO_DATE('2007/08/09', 'YYYY/MM/DD')");
-		expected.AppendLine("ELSE t.created_at");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SEARCH_CASE_Numeric_1()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					WHEN(_t.code == L(1)).THEN(L(2)),
-					ELSE(L(99))))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("WHEN (t.code = 1) THEN 2");
-		expected.AppendLine("ELSE 99");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SEARCH_CASE_Numeric_2()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					WHEN(_t.code == L(1)).THEN(L(2)),
-					WHEN(_t.code == L(3)).THEN(L(4)),
+					[
+						WHEN(_t.code == L(1)).THEN(L(2)),
+						WHEN(_t.code == L(3)).THEN(L(4)),
+					],
 					ELSE(L(99))))
 			.Build();
 
@@ -182,60 +80,16 @@ public class CaseTest
 	}
 
 	[Fact]
-	public void SELECT_SEARCH_CASE_Numeric_3()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					WHEN(_t.code == L(1)).THEN(L(2)),
-					WHEN(_t.code == L(3)).THEN(L(4)),
-					WHEN(_t.code == L(5)).THEN(L(6)),
-					ELSE(L(99))))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("WHEN (t.code = 1) THEN 2");
-		expected.AppendLine("WHEN (t.code = 3) THEN 4");
-		expected.AppendLine("WHEN (t.code = 5) THEN 6");
-		expected.AppendLine("ELSE 99");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SIMPLE_CASE_Character_1()
+	public void SELECT_SIMPLE_CASE_Character()
 	{
 		SqlCommand sql =
 			SELECT(
 				CASE(
 					_t.code,
-					WHEN(L(1)).THEN(L("a")),
-					ELSE(L("z"))))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("t.code");
-		expected.AppendLine("WHEN 1 THEN 'a'");
-		expected.AppendLine("ELSE 'z'");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SIMPLE_CASE_Character_2()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					_t.code,
-					WHEN(L(1)).THEN(L("a")),
-					WHEN(L(2)).THEN(L("b")),
+					[
+						WHEN(L(1)).THEN(L("a")),
+						WHEN(L(2)).THEN(L("b")),
+					],
 					ELSE(L("z"))))
 			.Build();
 
@@ -252,62 +106,16 @@ public class CaseTest
 	}
 
 	[Fact]
-	public void SELECT_SIMPLE_CASE_Character_3()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					_t.code,
-					WHEN(L(1)).THEN(L("a")),
-					WHEN(L(2)).THEN(L("b")),
-					WHEN(L(3)).THEN(L("c")),
-					ELSE(L("z"))))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("t.code");
-		expected.AppendLine("WHEN 1 THEN 'a'");
-		expected.AppendLine("WHEN 2 THEN 'b'");
-		expected.AppendLine("WHEN 3 THEN 'c'");
-		expected.AppendLine("ELSE 'z'");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SIMPLE_CASE_DateTime_1()
+	public void SELECT_SIMPLE_CASE_DateTime()
 	{
 		SqlCommand sql =
 			SELECT(
 				CASE(
 					_t.name,
-					WHEN(L("a")).THEN(TO_DATE(L("2001/02/03"), L("YYYY/MM/DD"))),
-					ELSE(_t.created_at)))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("t.name");
-		expected.AppendLine("WHEN 'a' THEN TO_DATE('2001/02/03', 'YYYY/MM/DD')");
-		expected.AppendLine("ELSE t.created_at");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SIMPLE_CASE_DateTime_2()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					_t.name,
-					WHEN(L("a")).THEN(TO_DATE(L("2001/02/03"), L("YYYY/MM/DD"))),
-					WHEN(L("b")).THEN(TO_DATE(L("2004/05/06"), L("YYYY/MM/DD"))),
+					[
+						WHEN(L("a")).THEN(TO_DATE(L("2001/02/03"), L("YYYY/MM/DD"))),
+						WHEN(L("b")).THEN(TO_DATE(L("2004/05/06"), L("YYYY/MM/DD"))),
+					],
 					ELSE(_t.created_at)))
 			.Build();
 
@@ -324,62 +132,16 @@ public class CaseTest
 	}
 
 	[Fact]
-	public void SELECT_SIMPLE_CASE_DateTime_3()
+	public void SELECT_SIMPLE_CASE_Numeric()
 	{
 		SqlCommand sql =
 			SELECT(
 				CASE(
 					_t.name,
-					WHEN(L("a")).THEN(TO_DATE(L("2001/02/03"), L("YYYY/MM/DD"))),
-					WHEN(L("b")).THEN(TO_DATE(L("2004/05/06"), L("YYYY/MM/DD"))),
-					WHEN(L("c")).THEN(TO_DATE(L("2007/08/09"), L("YYYY/MM/DD"))),
-					ELSE(_t.created_at)))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("t.name");
-		expected.AppendLine("WHEN 'a' THEN TO_DATE('2001/02/03', 'YYYY/MM/DD')");
-		expected.AppendLine("WHEN 'b' THEN TO_DATE('2004/05/06', 'YYYY/MM/DD')");
-		expected.AppendLine("WHEN 'c' THEN TO_DATE('2007/08/09', 'YYYY/MM/DD')");
-		expected.AppendLine("ELSE t.created_at");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SIMPLE_CASE_Numeric_1()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					_t.name,
-					WHEN(L("a")).THEN(L(1)),
-					ELSE(L(99))))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("t.name");
-		expected.AppendLine("WHEN 'a' THEN 1");
-		expected.AppendLine("ELSE 99");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SIMPLE_CASE_Numeric_2()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					_t.name,
-					WHEN(L("a")).THEN(L(1)),
-					WHEN(L("b")).THEN(L(2)),
+					[
+						WHEN(L("a")).THEN(L(1)),
+						WHEN(L("b")).THEN(L(2)),
+					],
 					ELSE(L(99))))
 			.Build();
 
@@ -389,32 +151,6 @@ public class CaseTest
 		expected.AppendLine("t.name");
 		expected.AppendLine("WHEN 'a' THEN 1");
 		expected.AppendLine("WHEN 'b' THEN 2");
-		expected.AppendLine("ELSE 99");
-		expected.Append("END");
-
-		Assert.Equal(expected.ToString(), sql.Statement);
-	}
-
-	[Fact]
-	public void SELECT_SIMPLE_CASE_Numeric_3()
-	{
-		SqlCommand sql =
-			SELECT(
-				CASE(
-					_t.name,
-					WHEN(L("a")).THEN(L(1)),
-					WHEN(L("b")).THEN(L(2)),
-					WHEN(L("c")).THEN(L(3)),
-					ELSE(L(99))))
-			.Build();
-
-		StringBuilder expected = new();
-		expected.AppendLine("SELECT");
-		expected.AppendLine("CASE");
-		expected.AppendLine("t.name");
-		expected.AppendLine("WHEN 'a' THEN 1");
-		expected.AppendLine("WHEN 'b' THEN 2");
-		expected.AppendLine("WHEN 'c' THEN 3");
 		expected.AppendLine("ELSE 99");
 		expected.Append("END");
 
