54a56ad261520b9eb867f04028375afaf08210d4
h-tacayama <h.tacayama@gmail.com>
2025-04-26 01:39:35 +0900

    perf: Eliminate delegation in SubstrbFunction to reduce allocations

--- src/InlineSqlSharp/SqlPart/Expr/Function/CharacterFunction/SubstrbFunction.cs
+++ src/InlineSqlSharp/SqlPart/Expr/Function/CharacterFunction/SubstrbFunction.cs
@@ -2,16 +2,25 @@ namespace InlineSqlSharp;
 
 public sealed class SubstrbFunction : AbstractExpr
 {
-    private readonly VariadicFunctionCore _core;
+    private readonly AbstractSqlPart _source;
+    private readonly AbstractSqlPart _position;
+    private readonly AbstractSqlPart? _length;
 
     internal SubstrbFunction(
         AbstractExpr source,
         AbstractExpr position,
         AbstractExpr? length = null)
     {
-        _core = new(Keywords.SUBSTRB, source, position, length);
+        _source = source;
+        _position = position;
+        _length = length;
     }
 
-    internal override void FormatSql(SqlBuildingBuffer buffer) =>
-        _core.FormatSql(buffer);
+    internal override void FormatSql(SqlBuildingBuffer buffer) => buffer
+        .Append(Keywords.SUBSTRB)
+        .OpenParenthesis()
+        .Append(_source)
+        .PrependComma(_position)
+        .PrependCommaIfNotNull(_length)
+        .CloseParenthesis();
 }
