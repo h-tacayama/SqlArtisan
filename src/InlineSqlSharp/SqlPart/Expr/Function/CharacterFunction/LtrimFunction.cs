a5f48796d2a9fdd6c9da87548b9dd94671e7d0c7
h-tacayama <h.tacayama@gmail.com>
2025-04-26 01:42:54 +0900

    perf: Eliminate delegation in LtrimFunction to reduce allocations

--- src/InlineSqlSharp/SqlPart/Expr/Function/CharacterFunction/LtrimFunction.cs
+++ src/InlineSqlSharp/SqlPart/Expr/Function/CharacterFunction/LtrimFunction.cs
@@ -2,15 +2,21 @@ namespace InlineSqlSharp;
 
 public sealed class LtrimFunction : AbstractExpr
 {
-    private readonly VariadicFunctionCore _core;
+    private readonly AbstractSqlPart _source;
+    private readonly AbstractSqlPart? _trimChars;
 
     internal LtrimFunction(
         AbstractExpr source,
         AbstractExpr? trimChars = null)
     {
-        _core = new(Keywords.LTRIM, source, trimChars);
+        _source = source;
+        _trimChars = trimChars;
     }
 
-    internal override void FormatSql(SqlBuildingBuffer buffer) =>
-        _core.FormatSql(buffer);
+    internal override void FormatSql(SqlBuildingBuffer buffer) => buffer
+        .Append(Keywords.LTRIM)
+        .OpenParenthesis()
+        .Append(_source)
+        .PrependCommaIfNotNull(_trimChars)
+        .CloseParenthesis();
 }
