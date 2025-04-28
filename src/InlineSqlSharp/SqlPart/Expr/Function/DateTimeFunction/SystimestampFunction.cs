ee34b840dab9d27a8d30672a15f46b68f72ada06
h-tacayama <h.tacayama@gmail.com>
2025-04-29 00:02:22 +0900

    chore: Configure Git to respect file name case sensitivity

--- src/InlineSqlSharp/SqlPart/Expr/Function/DateTimeFunction/SystimestampFunction.cs
+++
@@ -1,9 +0,0 @@
-namespace InlineSqlSharp;
-
-public sealed class SysTimestampFunction : AbstractExpr
-{
-    internal SysTimestampFunction() { }
-
-    internal override void FormatSql(SqlBuildingBuffer buffer) =>
-        buffer.Append(Keywords.SysTimestamp);
-}
