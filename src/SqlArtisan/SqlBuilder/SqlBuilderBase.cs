6b0c5e3489395bb749cb4469ecb83c2228100eb1
h-tacayama <h.tacayama@gmail.com>
2025-05-08 22:26:15 +0900

    perf: Use ArrayPool in SqlBuildingBuffer to reduce memory allocations

--- src/SqlArtisan/SqlBuilder/SqlBuilderBase.cs
+++ src/SqlArtisan/SqlBuilder/SqlBuilderBase.cs
@@ -1,19 +1,22 @@
 ﻿namespace SqlArtisan;
 
-internal abstract class SqlBuilderBase(SqlPart clause)
+internal abstract class SqlBuilderBase(SqlPart part)
 {
-    private readonly List<SqlPart> _clauses = [clause];
+    private readonly List<SqlPart> _parts = [part];
 
     protected void AddPart(SqlPart part)
     {
-        _clauses.Add(part);
+        _parts.Add(part);
     }
 
-    protected SqlStatement BuildCore() =>
-        new SqlBuildingBuffer()
-        .AppendSpaceSeparated(_clauses)
-        .ToSqlStatement();
+    protected SqlStatement BuildCore()
+    {
+        using SqlBuildingBuffer buffer = new();
+        return buffer
+            .AppendSpaceSeparated(_parts)
+            .ToSqlStatement();
+    }
 
     internal void Format(SqlBuildingBuffer buffer) =>
-        buffer.AppendSpaceSeparated(_clauses);
+        buffer.AppendSpaceSeparated(_parts);
 }
