abf85d3a29050fe3381d6065db406ec75f9259c2
h-tacayama <h.tacayama@gmail.com>
2026-06-08 22:36:53 +0900

    test: Order multi-row insert tests from simple to complex

--- tests/SqlArtisan.Tests/MultiRowInsertTests.cs
+++ tests/SqlArtisan.Tests/MultiRowInsertTests.cs
@@ -8,6 +8,24 @@ public class MultiRowInsertTests
     private readonly TestTable _t = new();
 
     [Fact]
+    public void Values_SingleRow_CorrectSql()
+    {
+        // Arrange
+        StringBuilder expected = new();
+        expected.Append("INSERT INTO test_table (code, name) ");
+        expected.Append("VALUES (:0, :1)");
+
+        // Act
+        SqlStatement sql =
+            InsertInto(_t, _t.Code, _t.Name)
+            .Values(1, "a")
+            .Build();
+
+        // Assert
+        Assert.Equal(expected.ToString(), sql.Text);
+    }
+
+    [Fact]
     public void Values_TwoRows_CorrectSql()
     {
         // Arrange
@@ -40,24 +58,6 @@ public class MultiRowInsertTests
             .Values(1, "a")
             .Values(2, "b")
             .Values(3, "c")
-            .Build();
-
-        // Assert
-        Assert.Equal(expected.ToString(), sql.Text);
-    }
-
-    [Fact]
-    public void Values_SingleRow_CorrectSql()
-    {
-        // Arrange
-        StringBuilder expected = new();
-        expected.Append("INSERT INTO test_table (code, name) ");
-        expected.Append("VALUES (:0, :1)");
-
-        // Act
-        SqlStatement sql =
-            InsertInto(_t, _t.Code, _t.Name)
-            .Values(1, "a")
             .Build();
 
         // Assert
